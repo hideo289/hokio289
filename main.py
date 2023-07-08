e54d669907aea20e1c67b10de2f0d857df0938ea
hideo289 <petovna229@gmail.com>
2023-07-08 19:44:36 +0300

    2 save

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from aiogram import Bot,Dispatcher,executor
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import config
 
+bot = Bot(token=config.TOKEN_API)
+dp = Dispatcher(bot)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+async def on_startup(dp):
+    print("I started")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    executor.start_polling(dp,on_startup=on_startup)
